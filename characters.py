dde009d39af28b3fcd1cba76687009b0119292a6
theetonip <tocoli52@gmail.com>
2021-07-02 12:14:08 -0400

    Characters chosen. Fight happens. Winner decided.

--- characters.py
+++ characters.py
@@ -1,76 +1,61 @@
-#Description: Character Class
 import random 
 
 class Character: 
+    #We use self b/c the object is referring to itself
     def __init__(self,name,weapon): 
         self.name = name 
         self.weapon = weapon
         #self.inventory = [] 
 
+    #GETTERS 
     def get_name(self): 
         return self.name
 
     def get_weapon(self): 
         return self.weapon
-
-    #Each function happens to charcters, hence their placement in the character class
-    '''def create_characters(self):
-        #Characters
-        Malik = Character("Malik", "Sword")
-        Lucina = Character ("Lucina", "Sword")
-        Vaike = Character("Vaike", "Axe")
-        Bob = Character("Bob","Axe")
-        Tera = Character("Tera", "Lance")
-        Sumia = Character("Sumia", "Lance")
-        character_list = [Malik, Lucina,Vaike,Bob,Tera,Sumia]
-        return character_list'''
     
-    #We use self b/c the object is referring to itself 
-    def start_battle(self):
-        character_list = self.create_characters()
-        #Set teams later! 
-        #COPIED AND PASTED!!! characters_chosen = random.choices(character_list, k=2)
-        pass 
-        #return characters_chosen #just an experiment 
-
-def main():
-    Malik = Character("Malik", "Sword")
-    Lucina = Character ("Lucina", "Sword")
-    Vaike = Character("Vaike", "Axe")
-    Bob = Character("Bob","Axe")
-    Tera = Character("Tera", "Lance")
-    Sumia = Character("Sumia", "Lance")
-
-    #saying hi :) 
-    print("WELCOME TO FIRE EMBLEM LITE *IMAGINE SOUND EFFECTS*")
-    character_list = [Malik, Lucina,Vaike,Bob,Tera,Sumia]
+#Characters
+MALIK = Character("Malik", "Sword")
+LUCINA = Character ("Lucina", "Sword")
+VAIKE= Character("Vaike", "Axe")
+BOB = Character("Bob","Axe")
+TERA = Character("Tera", "Lance")
+SUMIA = Character("Sumia", "Lance")
+character_list = [MALIK, LUCINA,VAIKE,BOB,TERA,SUMIA]
+     
+def character_select():
     char_1, char_2  = random.choices(character_list, k=2)
     while char_1 == char_2:
         char_1, char_2  = random.choices(character_list, k=2)
-    print("characters chosen: {} & {}".format(char_1.get_name(), char_2.get_name()))
+    print("In this round, {} will fight {}!".format(char_1.get_name(), char_2.get_name()))
+    return char_1, char_2
+
+def main():
+    #saying hi :) 
+    print("WELCOME TO FIRE EMBLEM LITE *IMAGINE SOUND EFFECTS*")
+
+    #character 1 and 2 selected
+    char_1, char_2 = character_select()
+
+   # weapon triangle!
+    if (char_1.get_weapon() == "Sword") and (char_2.get_weapon() == "Axe"): 
+        print (char_1.get_name(), " won!") 
+    elif (char_1.get_weapon() =="Axe") and (char_2.get_weapon() == "Lance"): 
+        print(char_1.get_name(), " won!")
+    elif (char_1.get_weapon() =="Lance") and (char_2.get_weapon() == "Sword"): 
+        print(char_1.get_name(), " won!")
+    elif (char_2.get_weapon() == "Sword") and (char_1.get_weapon() == "Axe"): 
+        print (char_2.get_name(), " won!") 
+    elif (char_2.get_weapon() =="Axe") and (char_1.get_weapon() == "Lance"): 
+        print(char_2.get_name(), " won!")
+    elif (char_2.get_weapon() =="Lance") and (char_1.get_weapon() == "Sword"): 
+        print(char_2.get_name(), " won!")
+    else:
+        print("No one won...It was a draw...")
+
+    #new problem: what if they have the same weapon! --> It's a draw atm :? (Fix when we add levels and health points)
+ 
+
 
 if __name__ == "__main__":
     main()
-
-
-'''
-Old random Player Selection
-
-def players():
-    "selects a character at random"
-    characters = ["Malik","Bob","Tera"]
-    rand1 = random.randint(0,2) 
-    rand2 = random.randint(0,2)
-   
-    #note: A person can't fight themselves 
-    if rand1 != rand2:
-        player1 = characters[rand1]
-        player2 = characters[rand2]
-    else: 
-        rand1 = random.randint(0,2) 
-        rand2 = random.randint(0,2)
-        player1 = characters[rand1]
-        player2 = characters[rand2]
-
-    return player1, player2  
-'''
